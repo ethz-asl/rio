9c3ba91e2e5b2ce8e99c1736d91eb84806b06519
Rik Bähnemann <brik@ethz.ch>
2023-07-19 21:46:55 +0200

    Export nodelet properly

---
+++ rio/include/rio/ros/baro.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <mav_sensors_drivers/barometer/bmp390.h>
+
+#include "rio/ros/base_sensor.h"
+
+namespace rio {
+class Baro : public BaseSensor {
+ private:
+  void readSensor() override;
+  bool openSensor() override;
+
+  BMP390<Spi> baro_;
+  ros::Publisher baro_pub_;
+  ros::Publisher temp_pub_;
+
+ public:
+  Baro(const ros::NodeHandle& nh_private);
+  ~Baro();
+};
+}  // namespace rio
