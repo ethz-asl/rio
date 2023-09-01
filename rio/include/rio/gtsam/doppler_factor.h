de6a7831b92c7de1ad62aeacd0fc4400a2e224fb
Rik Girod <brik@ethz.ch>
2023-09-01 15:52:57 +0200

    print, clone, equals

--- rio/include/rio/gtsam/doppler_factor.h
+++ rio/include/rio/gtsam/doppler_factor.h
@@ -1,32 +1,48 @@
 #pragma once
 
+#include <gtsam/base/Matrix.h>
 #include <gtsam/base/Vector.h>
 #include <gtsam/geometry/Pose3.h>
 #include <gtsam/linear/NoiseModel.h>
 #include <gtsam/navigation/ImuBias.h>
 #include <gtsam/nonlinear/NonlinearFactor.h>
-#include <log++.h>
 
 namespace rio {
 // Radar doppler velocity and direction factor.
-class DopplerFactor1
+class DopplerFactor
     : public gtsam::NoiseModelFactor3<gtsam::Pose3, gtsam::Vector,
                                       gtsam::imuBias::ConstantBias> {
  public:
-  DopplerFactor1(gtsam::Key I_T_I_B_key, gtsam::Key I_v_IB_key,
-                 gtsam::Key bias_key, const gtsam::Vector& R_r_RT_measured,
-                 const double doppler_measured,
-                 const gtsam::Vector& B_omega_IB_measured,
-                 const gtsam::Pose3& B_T_BR,
-                 const gtsam::noiseModel::Base::shared_ptr& noise_model);
+  DopplerFactor(gtsam::Key I_T_I_B_key, gtsam::Key I_v_IB_key,
+                gtsam::Key bias_key, const gtsam::Vector& R_r_RT_measured,
+                const double doppler_measured,
+                const gtsam::Vector& B_omega_IB_measured,
+                const gtsam::Pose3& B_T_BR,
+                const gtsam::noiseModel::Base::shared_ptr& noise_model);
 
-  gtsam::Vector unwhitenedError(const gtsam::Values& x,
-                                boost::optional<std::vector<gtsam::Matrix>&> H =
-                                    boost::none) const override;
+  gtsam::Vector evaluateError(
+      const gtsam::Pose3& I_T_I_B, const gtsam::Vector& I_v_IB,
+      const gtsam::imuBias::ConstantBias& bias,
+      boost::optional<gtsam::Matrix&> H_T = boost::none,
+      boost::optional<gtsam::Matrix&> H_v = boost::none,
+      boost::optional<gtsam::Matrix&> H_b = boost::none) const override;
 
   void print(const std::string& s,
              const gtsam::KeyFormatter& key_formatter =
                  gtsam::DefaultKeyFormatter) const override;
+
+  inline gtsam::NonlinearFactor::shared_ptr clone() const override {
+    return gtsam::NonlinearFactor::shared_ptr(new This(*this));
+  }
+
+  bool equals(const gtsam::NonlinearFactor& expected,
+              double tolerance) const override;
+
+ protected:
+  typedef gtsam::NoiseModelFactor3<gtsam::Pose3, gtsam::Vector,
+                                   gtsam::imuBias::ConstantBias>
+      Base;
+  typedef DopplerFactor This;
 
  private:
   gtsam::Vector R_r_RT_measured_;
