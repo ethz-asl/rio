df58403265fb1889ecfce91504e39e68104061a1
Rik Bähnemann <brik@ethz.ch>
2023-09-04 12:30:16 +0200

    first compiling complete factor error

--- rio/src/gtsam/doppler_factor.cpp
+++ rio/src/gtsam/doppler_factor.cpp
@@ -6,8 +6,8 @@ using namespace rio;
 
 DopplerFactor::DopplerFactor(
     gtsam::Key I_T_I_B_key, gtsam::Key I_v_IB_key, gtsam::Key bias_key,
-    const gtsam::Vector& R_r_RT_measured, const double doppler_measured,
-    const gtsam::Vector& B_omega_IB_measured, const gtsam::Pose3& B_T_BR,
+    const gtsam::Vector3& R_r_RT_measured, const double doppler_measured,
+    const gtsam::Vector3& B_omega_IB_measured, const gtsam::Pose3& B_T_BR,
     const gtsam::noiseModel::Base::shared_ptr& noise_model)
     : R_r_RT_measured_(R_r_RT_measured.normalized()),
       doppler_measured_(doppler_measured),
@@ -15,23 +15,52 @@ DopplerFactor::DopplerFactor(
       B_T_BR_(B_T_BR) {}
 
 gtsam::Vector DopplerFactor::evaluateError(
-    const gtsam::Pose3& I_T_I_B, const gtsam::Vector& I_v_IB,
+    const gtsam::Pose3& I_T_IB, const gtsam::Vector3& I_v_IB,
     const gtsam::imuBias::ConstantBias& bias,
     boost::optional<gtsam::Matrix&> H_T, boost::optional<gtsam::Matrix&> H_v,
     boost::optional<gtsam::Matrix&> H_b) const {
   // e(T, v, b) = [R_BR^-1 (R_IB^-1 * I_v_IB + (B_omega_IB - bias_omega) x
   // B_r_BR)]^T * R_r_RT_measured / ||R_r_RT_measured|| - doppler_measured
-  gtsam::Vector1 e;
-  if (H_T) H_T->resize(3, 6);
-  if (H_v) H_v->resize(3, 3);
-  if (H_b) H_b->resize(3, 6);
+  // Expanded:
+  // e(T, v, b) = I_v_IB^T * R_IB * R_BR * R_r_RT_measured / ||R_r_RT_measured||
+  // + B_r_BR^T [(B_omega_IB - bias_omega)]x^T * R_BR * R_r_RT_measured /
+  // ||R_r_RT_measured||
+  // - doppler_measured
 
-  if (H_T || H_v || H_b) {
-      // e = 
-    I_T_I_B.rotation().unrotate(gtsam::Point3(I_v_IB), &H_T->leftCols<3>(), H_v) ;
+  auto radar_proj_body = B_T_BR_.rotation().rotate(R_r_RT_measured_);
+  auto radar_proj_body_leverarm =
+      radar_proj_body.transpose() *
+      gtsam::skewSymmetric(B_T_BR_.translation().x(), B_T_BR_.translation().y(),
+                           B_T_BR_.translation().z())
+          .transpose();
+
+  gtsam::Matrix H_R_IB_raw, H_I_v_IB_raw, H_b_raw;
+  H_R_IB_raw.resize(3, 3);
+  H_I_v_IB_raw.resize(3, 3);
+  H_b_raw.resize(3, 6);
+  auto e_v = radar_proj_body.dot(
+      I_T_IB.rotation().unrotate(I_v_IB, &H_R_IB_raw, &H_I_v_IB_raw));
+  auto e_omega = radar_proj_body_leverarm.dot(
+      bias.correctGyroscope(B_omega_IB_measured_, &H_b_raw, boost::none));
+
+  if (H_T) {
+    H_T->resize(1, 6);
+    H_T->setZero();
+    H_T->leftCols<3>() = radar_proj_body.transpose() * H_R_IB_raw;
   }
 
-  return e;
+  if (H_v) {
+    H_v->resize(1, 3);
+    *H_v = radar_proj_body.transpose() * H_I_v_IB_raw;
+  }
+
+  if (H_b) {
+    H_b->resize(1, 6);
+    H_b->setZero();
+    H_b->rightCols<3>() = radar_proj_body_leverarm.transpose() * H_b_raw;
+  }
+
+  return gtsam::Vector1(e_v + e_omega - doppler_measured_);
 }
 
 void DopplerFactor::print(const std::string& text,
