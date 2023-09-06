e36da12bc27f2c58233d9287d79aa0ed72846653
Rik Girod <brik@ethz.ch>
2023-09-06 12:42:21 +0200

    Load integrator parameters

--- rio/src/ros/rio.cpp
+++ rio/src/ros/rio.cpp
@@ -1,8 +1,13 @@
 #include "rio/ros/rio.h"
 
+#include <cmath>
+
+#include <gtsam/navigation/CombinedImuFactor.h>
 #include <log++.h>
 #include <nav_msgs/Odometry.h>
 #include <tf2_eigen/tf2_eigen.h>
+
+#include "rio/ros/common.h"
 
 using namespace rio;
 using namespace gtsam;
@@ -13,12 +18,11 @@ Rio::Rio(const ros::NodeHandle& nh, const ros::NodeHandle& nh_private)
 Rio::~Rio() {}
 
 bool Rio::init() {
-  int32_t queue_size = 1;
-  if (!nh_private_.getParam("queue_size", queue_size)) {
-    LOG(F, "Failed to read queue_size.");
-    return false;
-  }
+  // ROS communication.
+  int queue_size = 1;
+  if (!loadParam<int>(nh_private_, "queue_size", &queue_size)) return false;
 
+  // Subscribers.
   imu_raw_sub_ =
       nh_.subscribe("imu/data_raw", queue_size, &Rio::imuRawCallback, this);
   imu_filter_sub_ =
@@ -28,10 +32,60 @@ bool Rio::init() {
   radar_cfar_sub_ = nh_.subscribe("radar/cfar_detections", queue_size,
                                   &Rio::cfarDetectionsCallback, this);
 
+  // Publishers
   odom_integrated_pub_ = nh_private_.advertise<nav_msgs::Odometry>(
       "odometry_integrated", queue_size);
   odom_optimized_pub_ = nh_private_.advertise<nav_msgs::Odometry>(
       "odometry_optimized", queue_size);
+
+  // IMU integration
+  double bias_acc_sigma = 0.0, bias_omega_sigma = 0.0, bias_acc_int_sigma = 0.0,
+         bias_omega_int_sigma = 0.0, acc_sigma = 0.0, integration_sigma = 0.0,
+         gyro_sigma = 0.0;
+  // TODO: Possibly expose "use2ndOrderCoriolis", "omegaCoriolis",
+  // "body_P_sensor" and "n_gravity" as parameters. But only really makes sense
+  // if we have a earth-centered coordinate frame or not a IMU centered frame.
+  if (!loadParam<double>(nh_private_, "imu/bias_acc_sigma", &bias_acc_sigma))
+    return false;
+  if (!loadParam<double>(nh_private_, "imu/bias_omega_sigma",
+                         &bias_omega_sigma))
+    return false;
+  if (!loadParam<double>(nh_private_, "imu/bias_acc_int_sigma",
+                         &bias_acc_int_sigma))
+    return false;
+  if (!loadParam<double>(nh_private_, "imu/bias_omega_int_sigma",
+                         &bias_omega_int_sigma))
+    return false;
+  if (!loadParam<double>(nh_private_, "imu/acc_sigma", &acc_sigma))
+    return false;
+  if (!loadParam<double>(nh_private_, "imu/integration_sigma",
+                         &integration_sigma))
+    return false;
+  if (!loadParam<double>(nh_private_, "imu/gyro_sigma", &gyro_sigma))
+    return false;
+
+  auto imu_params_ = PreintegratedCombinedMeasurements::Params::MakeSharedU();
+  imu_params_->biasAccCovariance = I_3x3 * std::pow(bias_acc_sigma, 2);
+  imu_params_->biasOmegaCovariance = I_3x3 * std::pow(bias_omega_sigma, 2);
+  imu_params_->biasAccOmegaInt.block<3, 3>(0, 0) =
+      I_3x3 * std::pow(bias_acc_int_sigma, 2);
+  imu_params_->biasAccOmegaInt.block<3, 3>(3, 3) =
+      I_3x3 * std::pow(bias_omega_int_sigma, 2);
+
+  imu_params_->accelerometerCovariance = I_3x3 * std::pow(acc_sigma, 2);
+  imu_params_->integrationCovariance = I_3x3 * std::pow(integration_sigma, 2);
+  imu_params_->gyroscopeCovariance = I_3x3 * std::pow(gyro_sigma, 2);
+
+  if (!loadParam<std::optional<Vector3>>(nh_private_, "imu/initial_bias_acc",
+                                         &initial_state_.b_a))
+    return false;
+  if (!loadParam<std::optional<Vector3>>(nh_private_, "imu/initial_bias_gyro",
+                                         &initial_state_.b_g))
+    return false;
+  integrator_ = PreintegratedCombinedMeasurements(
+      imu_params_, {initial_state_.b_a.value(), initial_state_.b_g.value()});
+  integrator_.print("Initial preintegration parameters: ");
+
   return true;
 }
 
