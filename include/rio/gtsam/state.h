d2b1fd85a03df569c219567d453622f05fe7e246
Rik Girod <brik@ethz.ch>
2023-09-07 16:03:07 +0200

    state omega and relax getters

--- include/rio/gtsam/state.h
+++ include/rio/gtsam/state.h
@@ -3,23 +3,27 @@
 #include <gtsam/base/Vector.h>
 #include <gtsam/geometry/Rot3.h>
 #include <gtsam/navigation/ImuBias.h>
+#include <gtsam/navigation/NavState.h>
 #include <nav_msgs/Odometry.h>
 #include <ros/time.h>
-#include <gtsam/navigation/NavState.h>
 
 namespace rio {
 struct State {
   std::optional<ros::Time> stamp;
   std::optional<std::string> odom_frame_id;
   std::optional<std::string> body_frame_id;
+
+  // State variables.
   std::optional<gtsam::Point3> I_p_IB;
   std::optional<gtsam::Rot3> R_IB;
   std::optional<gtsam::Vector3> I_v_IB;
   std::optional<gtsam::Vector3> b_a;
   std::optional<gtsam::Vector3> b_g;
 
-  bool isComplete() const;
-  bool reset();
+  // Measurements.
+  std::optional<gtsam::Vector3> B_omega_IB;
+
+  bool hasFullState() const;
   nav_msgs::Odometry getOdometry() const;
   geometry_msgs::TransformStamped getTransform() const;
   geometry_msgs::Vector3Stamped getBiasAcc() const;
