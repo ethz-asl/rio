a63948a2133aa8375dbdff055ec3b4c5a08147a3
Rik Bähnemann <brik@ethz.ch>
2023-10-17 16:57:27 +0200

    Include expressions in optimization

--- rio/include/rio/gtsam/expressions.h
+++ rio/include/rio/gtsam/expressions.h
@@ -2,10 +2,13 @@
 
 #include <gtsam/geometry/Point3.h>
 #include <gtsam/nonlinear/expressions.h>
+#include <gtsam/slam/expressions.h>
+
+#include "rio/gtsam/functions.h"
 
 namespace rio {
-inline gtsam::Double_ radialVelocity_(const gtsam::Vector3& R_v_IR,
-                                      const gtsam::Point3& R_p_RT) {
-  return Double_(&radialVelocity, R_v_IR, R_p_RT);
+inline gtsam::Double_ radialVelocity_(const gtsam::Vector3_& R_v_IR,
+                                      const gtsam::Point3_& R_p_RT) {
+  return gtsam::Double_(&radialVelocity, R_v_IR, R_p_RT);
 }
 }  // namespace rio
