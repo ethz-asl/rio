ba5ea8d10ffa15527e26722289db977e64c45568
Rik Bähnemann <brik@ethz.ch>
2024-01-27 19:33:22 +0100

    layout

--- script/plot_velocities_robust_doppler.py
+++ script/plot_velocities_robust_doppler.py
@@ -22,23 +22,23 @@ f_velocities = []
 h_velocities = []
 c_velocities = []
 w_velocities = []
-start = 1705256242.5
-duration = 10 # window size
+start = 1705256241.5
+duration = 12 # window size
 for topic, msg, t in q_bag.read_messages(topics=[topic]):
     if msg.header.stamp.to_sec() > start and msg.header.stamp.to_sec() < start+duration:
-        q_velocities.append([msg.header.stamp.to_sec() - start, np.linalg.norm([msg.twist.twist.linear.x, msg.twist.twist.linear.y, msg.twist.twist.linear.z])])
+        q_velocities.append([msg.header.stamp.to_sec() - start-1, np.linalg.norm([msg.twist.twist.linear.x, msg.twist.twist.linear.y, msg.twist.twist.linear.z])])
 for topic, msg, t in f_bag.read_messages(topics=[topic]):
     if msg.header.stamp.to_sec() > start and msg.header.stamp.to_sec() < start+duration:
-        f_velocities.append([msg.header.stamp.to_sec() - start, np.linalg.norm([msg.twist.twist.linear.x, msg.twist.twist.linear.y, msg.twist.twist.linear.z])])
+        f_velocities.append([msg.header.stamp.to_sec() - start-1, np.linalg.norm([msg.twist.twist.linear.x, msg.twist.twist.linear.y, msg.twist.twist.linear.z])])
 for topic, msg, t in h_bag.read_messages(topics=[topic]):
     if msg.header.stamp.to_sec() > start and msg.header.stamp.to_sec() < start+duration:
-        h_velocities.append([msg.header.stamp.to_sec() - start, np.linalg.norm([msg.twist.twist.linear.x, msg.twist.twist.linear.y, msg.twist.twist.linear.z])])
+        h_velocities.append([msg.header.stamp.to_sec() - start-1, np.linalg.norm([msg.twist.twist.linear.x, msg.twist.twist.linear.y, msg.twist.twist.linear.z])])
 for topic, msg, t in c_bag.read_messages(topics=[topic]):
     if msg.header.stamp.to_sec() > start and msg.header.stamp.to_sec() < start+duration:
-        c_velocities.append([msg.header.stamp.to_sec() - start, np.linalg.norm([msg.twist.twist.linear.x, msg.twist.twist.linear.y, msg.twist.twist.linear.z])])
+        c_velocities.append([msg.header.stamp.to_sec() - start-1, np.linalg.norm([msg.twist.twist.linear.x, msg.twist.twist.linear.y, msg.twist.twist.linear.z])])
 for topic, msg, t in w_bag.read_messages(topics=[topic]):
     if msg.header.stamp.to_sec() > start and msg.header.stamp.to_sec() < start+duration:
-        w_velocities.append([msg.header.stamp.to_sec() - start, np.linalg.norm([msg.twist.twist.linear.x, msg.twist.twist.linear.y, msg.twist.twist.linear.z])])
+        w_velocities.append([msg.header.stamp.to_sec() - start-1, np.linalg.norm([msg.twist.twist.linear.x, msg.twist.twist.linear.y, msg.twist.twist.linear.z])])
 # Plot velocities
 plt.figure(figsize=(3.5, 1.6))
 next(plt.gca()._get_lines.prop_cycler)
@@ -54,6 +54,7 @@ plt.ylabel(r'$\displaystyle \lVert {_I\mathbf{v}_{IB}} \rVert ~ [ms^{-1}]$')
 plt.gca().set_yscale('log')
 #plt.legend(loc='lower right',prop={'size': 8})
 plt.grid()
+plt.xlim(0, 10)
 #plt.show()
 
 plt.tight_layout()
